5601160e26de4dd59af66f6efd3f904af67d520e
Andrey Riça <the-Andrey>
2024-01-24 09:58:03 -0400

    Zoom in and zoom out functions created

---
+++ backend/improc/zoom_in.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2 as cv
+import random
+
+
+test = "/home/andrey/Pasta_PDI/imagens/legumes.tiff"
+img = cv.imread(test)
+
+
+factor = 1
+
+
+def zoom_in_replication(img:np.ndarray, factor:float)->np.ndarray:
+    row,col,ch = img.shape
+    
+    new_row = int(row * factor)
+    new_col = int(col * factor)
+
+
+    output:np.ndarray = np.zeros((new_row,new_col,ch), dtype=img.dtype)
+    
+    for i in range(new_row):
+        for j in range(new_col):
+
+            origin_row = int(i / factor)
+            origin_col = int(j / factor)
+
+            origin_row = min(origin_row, row -1)
+            origin_col = min(origin_col, col - 1)
+
+
+            
+            output[i,j,:] = img[origin_row,origin_col,:]
+
+    
+    return output      
+        
+
+output = zoom_in_replication(img,3)
+
+cv.imshow("image",img)
+cv.imshow("Output",output)
+cv.waitKey(0)
+cv.destroyAllWindows()
