f3c4f1ea81959d041eb7f12eb39a4f9e679c8110
realnfcs <nicolas.cavalcante.dev@gmail.com>
2024-01-30 22:05:00 -0400

    initial frontend setup

---
+++ frontend/src/components/navbar/NavBar.jsx
@@ -0,0 +1,15 @@
+import './NavBar.css'
+
+function NavBar() {
+  return(
+    <div>
+      <nav>
+        <div className="nav-container">
+          <p>Navbar</p>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -0,0 +1,11 @@
+import './SideBar.css'
+
+function SideBar() {
+  return (
+    <div className="sidebar-container">
+      <aside>SideBar</aside>
+    </div>
+  )
+}
+
+export default SideBar
